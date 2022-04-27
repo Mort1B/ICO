1ca25084f6aef8f1b5c90598589661abe7b648bb
Morten Bergseng <bergseng.m@gmail.com>
2022-04-27 13:37:11 +0200

    mosly

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xC98d1a9C748A638f49219a437830566f158070d9";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
